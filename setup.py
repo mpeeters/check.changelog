a8b51fceeb1a33f46566898bdd1dba0268fafc5a
Martin Peeters <martin.peeters@affinitic.be>
2014-05-04 13:34:38 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+from setuptools import find_packages
+
+version = '0.1'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+setup(name='check.changelog',
+      version=version,
+      description="Check changelog from packages",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      keywords='',
+      author='Martin Peeters',
+      author_email='martin.peeters@affinitic.be',
+      url='https://github.com/mpeeters/check.changelog',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['check'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ])
